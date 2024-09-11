14d636a08619242ecf4de7fb03abb26dd4c4400d
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-09-11 16:56:17 -0400

    hash.h created (in class)

---
+++ HW_1/hash.cpp
@@ -0,0 +1,4 @@
+//Implement a hashmap in cpp, using linear probing to avoid hash collisions. 
+//Use void pointers instead of templates.
+//Dictionary size is not known: create a rehash function.
+//
---
+++ HW_1/hash.h
@@ -0,0 +1,53 @@
+#ifndef _HASH_H
+#define _HASH_H
+#include <string>
+#include <vector>
+
+class hashTable {
+    public:
+    //Hash Table constructor 
+    hashTable(int size = 0);
+
+    //if pointer is provided, associate pointer with key
+    //0 if success, 1 if key already exists, 2 if rehash fails.
+    int insert(const std::string &key, void *pv = nullptr);
+
+    //true if key in hash
+    //false otherwise
+    bool contains(const std::string &key);
+
+    //return nullptr if no key
+    //if pointer to bool is provided, set bool to true if pointer is provided
+    //set bool to false otherwise
+    void *getPointer(const std::string &key, bool *b = nullptr);
+
+    //True if deletion is successful, False if key is not there
+    bool remove(const std::string &key);
+    private:
+        //every object in a hash table. key is the string used as a key
+        //isOccupied tells you that this entry is empty
+        //isDeleted tells you this element is free to be replaced, 
+        //and for linear probing, that searching should continue
+        //pv is a pointer to the value associated with the key, null if no value.
+        class hashItem {
+            public:
+                std::string key {""};
+                bool isOccupied {false};
+                bool isDeleted {false};
+                void *pv {nullptr};
+        };
+
+        //Hash generator for a given key
+        int hash(const std::string &key);
+        //Searches for an item with a given key
+        //Return position if found, otherwise -1
+        int findPos(const std::string &key);
+
+        //True on success, False if memory allocation fails
+        bool rehash();
+        //Return a prime number with lower bound size
+        //Can use a precomputed list of selected prime numbers.
+        static unsigned int getPrime(int size);
+};
+
+#endif _HASH_H
