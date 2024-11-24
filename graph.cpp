316a440193942e26ca7fbe900548c22642cf21cf
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-11-24 16:42:53 -0500

    Formatting everything.

--- graph.cpp
+++ graph.cpp
@@ -3,115 +3,115 @@
 graph::graph() : vertex_lookup(500) { num_elements = 0; }
 
 int graph::insert(std::string initial, std::string final, int weight) {
-  vertex *initial_vertex;
-  vertex *final_vertex;
+    vertex *initial_vertex;
+    vertex *final_vertex;
 
-  bool exists;
-  void *raw_pointer = vertex_lookup.getPointer(initial, &exists);
+    bool exists;
+    void *raw_pointer = vertex_lookup.getPointer(initial, &exists);
 
-  if (exists) {
-    initial_vertex = (vertex *)raw_pointer;
-  } else {
-    int status = 0;
-    initial_vertex = insert_new(initial, status);
-    if (status > 0) {
-      return status;
+    if (exists) {
+        initial_vertex = (vertex *)raw_pointer;
+    } else {
+        int status = 0;
+        initial_vertex = insert_new(initial, status);
+        if (status > 0) {
+            return status;
+        }
     }
-  }
-  raw_pointer = vertex_lookup.getPointer(final, &exists);
+    raw_pointer = vertex_lookup.getPointer(final, &exists);
 
-  if (exists) {
-    final_vertex = (vertex *)raw_pointer;
-  } else {
-    int status = 0;
-    final_vertex = insert_new(final, status);
-    if (status > 0) {
-      return status;
+    if (exists) {
+        final_vertex = (vertex *)raw_pointer;
+    } else {
+        int status = 0;
+        final_vertex = insert_new(final, status);
+        if (status > 0) {
+            return status;
+        }
     }
-  }
 
-  edge *newedge = new edge();
-  newedge->final = final_vertex;
-  newedge->weight = weight;
-  initial_vertex->paths.push_back(newedge);
-  return 0;
+    edge *newedge = new edge();
+    newedge->final = final_vertex;
+    newedge->weight = weight;
+    initial_vertex->paths.push_back(newedge);
+    return 0;
 }
 
 graph::vertex *graph::insert_new(std::string name, int &status) {
-  vertex *final_vertex = new vertex();
-  status = vertex_lookup.insert(name, final_vertex);
-  if (status > 0) {
-    return nullptr;
-  }
+    vertex *final_vertex = new vertex();
+    status = vertex_lookup.insert(name, final_vertex);
+    if (status > 0) {
+        return nullptr;
+    }
 
-  final_vertex->name = name;
-  final_vertex->position = num_elements;
-  adj_list.push_back(final_vertex);
-  num_elements++;
-  return final_vertex;
+    final_vertex->name = name;
+    final_vertex->position = num_elements;
+    adj_list.push_back(final_vertex);
+    num_elements++;
+    return final_vertex;
 }
 
 std::vector<std::string> graph::djikstra(std::string starting_node) {
-  // Quick lookups for each element: This allows for the final ordering to be
-  // proper
-  std::vector<int> weights;
-  weights.resize(num_elements);
-  std::vector<std::string> paths;
-  paths.resize(num_elements);
+    // Quick lookups for each element: This allows for the final ordering to be
+    // proper
+    std::vector<int> weights;
+    weights.resize(num_elements);
+    std::vector<std::string> paths;
+    paths.resize(num_elements);
 
-  heap Pqueue = heap(num_elements);
-  // hashTable return_table = hashTable(num_elements * 2);
-  bool exists = true;
-  void *raw_node = vertex_lookup.getPointer(starting_node, &exists);
-  if (!exists) {
-    return paths;
-  }
+    heap Pqueue = heap(num_elements);
+    // hashTable return_table = hashTable(num_elements * 2);
+    bool exists = true;
+    void *raw_node = vertex_lookup.getPointer(starting_node, &exists);
+    if (!exists) {
+        return paths;
+    }
 
-  vertex *initial_node = (vertex *)raw_node;
-  Pqueue.insert(initial_node->name, 0, raw_node);
-  int weight;
+    vertex *initial_node = (vertex *)raw_node;
+    Pqueue.insert(initial_node->name, 0, raw_node);
+    int weight;
 
-  paths[initial_node->position] = "[ " + initial_node->name;
-  weights[initial_node->position] = 0;
+    paths[initial_node->position] = "[ " + initial_node->name;
+    weights[initial_node->position] = 0;
 
-  while (Pqueue.deleteMin(nullptr, &weight, raw_node) < 1) {
-    vertex *current = (vertex *)raw_node;
-    weights[current->position] = weight;
-    for (edge *cur_edge : current->paths) {
-      vertex *next_vertex = cur_edge->final;
-      // 2 if element exists
-      if (Pqueue.insert(next_vertex->name, weight + cur_edge->weight,
-                        next_vertex) == 2) {
-        int existing_weight;
-        Pqueue.get_node(next_vertex->name, &existing_weight);
-        if (existing_weight > weight + cur_edge->weight) {
-          Pqueue.setKey(next_vertex->name, weight + cur_edge->weight);
-          paths[next_vertex->position] =
-              paths[current->position] + "," + next_vertex->name;
+    while (Pqueue.deleteMin(nullptr, &weight, raw_node) < 1) {
+        vertex *current = (vertex *)raw_node;
+        weights[current->position] = weight;
+        for (edge *cur_edge : current->paths) {
+            vertex *next_vertex = cur_edge->final;
+            // 2 if element exists
+            if (Pqueue.insert(next_vertex->name, weight + cur_edge->weight,
+                              next_vertex) == 2) {
+                int existing_weight;
+                Pqueue.get_node(next_vertex->name, &existing_weight);
+                if (existing_weight > weight + cur_edge->weight) {
+                    Pqueue.setKey(next_vertex->name, weight + cur_edge->weight);
+                    paths[next_vertex->position] =
+                        paths[current->position] + "," + next_vertex->name;
 
-          weights[next_vertex->position] = weight + cur_edge->weight;
+                    weights[next_vertex->position] = weight + cur_edge->weight;
+                }
+
+            } else {
+                paths[next_vertex->position] =
+                    paths[current->position] + ", " + next_vertex->name;
+                weights[next_vertex->position] = weight + cur_edge->weight;
+            }
         }
+    }
 
+    // Generating the final vector for display
+    std::vector<std::string> final_vec;
+    final_vec.resize(num_elements);
+    for (vertex *vertex : adj_list) {
+        final_vec[vertex->position] = vertex->name + ":";
+        if (paths[vertex->position].empty()) {
+            final_vec[vertex->position] += " NO PATH";
         } else {
-          paths[next_vertex->position] =
-              paths[current->position] + ", " + next_vertex->name;
-          weights[next_vertex->position] = weight + cur_edge->weight;
+            final_vec[vertex->position] +=
+                " " + std::to_string(weights[vertex->position]) + " " +
+                paths[vertex->position] + "]";
         }
-      }
     }
-
-  // Generating the final vector for display
-  std::vector<std::string> final_vec;
-  final_vec.resize(num_elements);
-  for (vertex *vertex : adj_list) {
-    final_vec[vertex->position] = vertex->name + ":";
-    if (paths[vertex->position].empty()) {
-      final_vec[vertex->position] += " NO PATH";
-    } else {
-      final_vec[vertex->position] += " " +
-                                     std::to_string(weights[vertex->position]) +
-                                     " " + paths[vertex->position] + "]";
-    }
-  }
-  return final_vec;
+    return final_vec;
 }
--- heap.h
+++ heap.h
@@ -6,91 +6,91 @@
 #include "hash.h"
 // Min heap: As we're trying to extract smallest element
 class heap {
- public:
-  // heap - The constructor allocates space for the nodes of the heap
-  // and the mapping (hash table) based on the specified capacity
-  //
-  heap(int capacity);
-  // insert - Inserts a new node into the binary heap
-  //
-  // Inserts a node with the specified id string, key,
-  // and optionally a pointer.  They key is used to
-  // determine the final position of the new node.
-  //
-  // Returns:
-  //   0 on success
-  //   1 if the heap is already filled to capacity
-  //   2 if a node with the given id already exists (but the heap
-  //     is not filled to capacity)
-  //
-  int insert(const std::string &id, int key, void *pv = nullptr);
+   public:
+    // heap - The constructor allocates space for the nodes of the heap
+    // and the mapping (hash table) based on the specified capacity
+    //
+    heap(int capacity);
+    // insert - Inserts a new node into the binary heap
+    //
+    // Inserts a node with the specified id string, key,
+    // and optionally a pointer.  They key is used to
+    // determine the final position of the new node.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if the heap is already filled to capacity
+    //   2 if a node with the given id already exists (but the heap
+    //     is not filled to capacity)
+    //
+    int insert(const std::string &id, int key, void *pv = nullptr);
 
-  //
-  // setKey - set the key of the specified node to the specified value
-  //
-  // I have decided that the class should provide this member function
-  // instead of two separate increaseKey and decreaseKey functions.
-  //
-  // Returns:
-  //   0 on success
-  //   1 if a node with the given id does not exist
-  //
-  int setKey(const std::string &id, int key);
+    //
+    // setKey - set the key of the specified node to the specified value
+    //
+    // I have decided that the class should provide this member function
+    // instead of two separate increaseKey and decreaseKey functions.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if a node with the given id does not exist
+    //
+    int setKey(const std::string &id, int key);
 
-  //
-  // deleteMin - return the data associated with the smallest key
-  //             and delete that node from the binary heap
-  //
-  // If pId is supplied (i.e., it is not nullptr), write to that address
-  // the id of the node being deleted. If pKey is supplied, write to
-  // that address the key of the node being deleted. If ppData is
-  // supplied, write to that address the associated void pointer.
-  //
-  // Returns:
-  //   0 on success
-  //   1 if the heap is empty
-  //
-  int deleteMin(std::string *pId = nullptr, int *pKey = nullptr,
-                void *ppData = nullptr);
+    //
+    // deleteMin - return the data associated with the smallest key
+    //             and delete that node from the binary heap
+    //
+    // If pId is supplied (i.e., it is not nullptr), write to that address
+    // the id of the node being deleted. If pKey is supplied, write to
+    // that address the key of the node being deleted. If ppData is
+    // supplied, write to that address the associated void pointer.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if the heap is empty
+    //
+    int deleteMin(std::string *pId = nullptr, int *pKey = nullptr,
+                  void *ppData = nullptr);
 
-  //
-  // remove - delete the node with the specified id from the binary heap
-  //
-  // If pKey is supplied, write to that address the key of the node
-  // being deleted. If ppData is supplied, write to that address the
-  // associated void pointer.
-  //
-  // Returns:
-  //   0 on success
-  //   1 if a node with the given id does not exist
-  //
-  int remove(const std::string &id, int *pKey = nullptr,
-             void *ppData = nullptr);
-
-  // New function to get node data given a key.
-  //  0 on success
-  //  1 if node does not exist
-  int get_node(const std::string &id, int *pKey = nullptr,
+    //
+    // remove - delete the node with the specified id from the binary heap
+    //
+    // If pKey is supplied, write to that address the key of the node
+    // being deleted. If ppData is supplied, write to that address the
+    // associated void pointer.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if a node with the given id does not exist
+    //
+    int remove(const std::string &id, int *pKey = nullptr,
                void *ppData = nullptr);
 
- private:
-  int capacity;
-  int num_elements;
-  class node {
-   public:
-    std::string id;
-    int key;
-    void *pData;
-  };
-  std::vector<node> data;
-  hashTable mapping;
+    // New function to get node data given a key.
+    //  0 on success
+    //  1 if node does not exist
+    int get_node(const std::string &id, int *pKey = nullptr,
+                 void *ppData = nullptr);
 
-  void extract_data(node &n, std::string *pId = nullptr, int *pKey = nullptr,
-                    void *ppData = nullptr);
+   private:
+    int capacity;
+    int num_elements;
+    class node {
+       public:
+        std::string id;
+        int key;
+        void *pData;
+    };
+    std::vector<node> data;
+    hashTable mapping;
 
-  void percolateUp(int posCur);
-  void percolateDown(int posCur);
-  int getPos(node *pn);
+    void extract_data(node &n, std::string *pId = nullptr, int *pKey = nullptr,
+                      void *ppData = nullptr);
+
+    void percolateUp(int posCur);
+    void percolateDown(int posCur);
+    int getPos(node *pn);
 };
 
 #endif  // HEAP_H_
